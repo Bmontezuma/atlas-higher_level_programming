f43c80be4a92bef7f72de84b44f3fefcd3ae28a7
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-03 02:11:32 -0800

    Pos or Neg

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,6 @@ import random
 # This will assign a random number to 'number'
 number = random.randint(-10, 10)
 
-# Print assigned number.
-print("The number:", number)
-
-# Check if number is positive, negative, or a zero.
 if number > 0:
     print("is positive")
 elif number == 0:
