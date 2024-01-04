868c73dd7e4af4371c356ab12185ad20773b9131
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-03 18:42:44 -0800

    Updated

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+num_args = len(argv) - 1
+
+print("{} argument{}{}.".format(num_args, 's' if num_args != 1 else '', ':' if num_args > 0 else '.'))
+
+for i, arg in enumerate(argv[1:], start=1):
+    print("{}: {}".format(i, arg))
