af02ef6215dbd51662a5b46c756b9cc5f469eeb8
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-04 17:18:44 -0800

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit, end="")
-    return last_digit
+    if isinstance(number, int):
+        last_digit = abs(number) % 10
+        print(last_digit, end="")
+        return last_digit
+    else:
+        raise TypeError("Input must be an integer")
