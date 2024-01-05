41fc887712d7b6c51ed9d1e348cc52fe359aa675
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-04 17:11:44 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for units_digit in range(tens_digit + 1, 10):
-        sep = ", " if units_digit < 9 else "\n"
-        print("{:d}{:d}".format(tens_digit, units_digit), end=sep)
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
