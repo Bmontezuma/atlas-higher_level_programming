f24d37448eb5d5b816b2532a32bec3b9bd4f140b
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-11 15:44:40 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+This is a module defining a square class.
+"""
+
+
+class Square:
+    """
+    This is a class representing a square.
+
+    Attributes:
+        __size (int): Attribute representing the size of the square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
