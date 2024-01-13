c5183a5e4bef0d368e055faaf8e63a257fd78cd9
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-13 10:35:12 -0800

    Updated

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module has function to print text with 2 new lines after each: ., ? and :
+"""
+
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each '.', '?', and ':'.
@@ -19,18 +24,22 @@ def text_indentation(text):
     current_line = ""
     prev_punct = False
 
+    # replace all whitespace characters with spaces
     text = text.replace("\\t", " ").replace("\\n", " ").replace("\\r", " ")
 
     for char in text:
         current_line += char
 
         if char in separators:
+            # print the current line without leading or trailing spaces
             print(current_line.strip())
             print()
             current_line = ""
             prev_punct = True
         else:
+            # reset the flag if the character is not a punctuation mark
             prev_punct = False
 
+    # print the remaining line if it is not empty or a punctuation mark
     if current_line and not prev_punct:
         print(current_line.strip())
