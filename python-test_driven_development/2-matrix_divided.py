5157ce717f56ef5e34651d02cc0cdaeaf846fc56
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-13 08:15:50 -0800

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
-"""
-This module is a function to divide all elements of matrix by a given number.
-"""
+"""This module contains a function to print square with the character #."""
 
 
-def matrix_divided(matrix, div):
+def print_square(size):
     """
-    This function divides all elements of matrix by the given number.
+    Print a square with the character #.
 
-    Parameters:
-    matrix (list of lists): The matrix to divide.
-    div (int or float): The number to divide the matrix elements by.
-
-    Returns:
-    list of lists of float: The matrix after division.
+    Args:
+        size (int): The size length of the square.
 
     Raises:
-    TypeError: If matrix is not a matrix (list of lists) of integers/floats,
-               or if each row of the matrix does not have the same size,
-               or if div is not a number (integer or float).
-    ZeroDivisionError: If div is equal to 0.
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Returns:
+        None
     """
-    if not all(isinstance(row, list) for row in matrix) or not all(
-            all(isinstance(num, (int, float)) for num in row) for row in matrix
-    ):
-        raise TypeError("matrix must be (list of lists) of integers/floats")
-    if len(set(len(row) for row in matrix)) != 1:
-        raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(float(num) / div, 2) for num in row] for row in matrix]
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
