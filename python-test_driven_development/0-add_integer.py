f2224dc0682b4850e020965983e52490b6433b5f
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-13 06:54:41 -0800

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Adds two integers.
+    """
+    This function adds two integers.
 
-    Args:
-        a (int or float): The first integer or float.
-        b (int or float): The second integer or float
+    Parameters:
+    a (int or float): The first number to add.
+    b (int or float, optional): The second number to add. Default is 98.
 
     Returns:
-        int: Addition of a and b
-
-    Raises:
-        TypeError: If not integer or float
+    int: The sum of a and b.
     """
 
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
 
     a = int(a)
     b = int(b)
 
     return a + b
-
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
