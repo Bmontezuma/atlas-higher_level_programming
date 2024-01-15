a206bff5912c170f16ec85123939295c6950f3ef
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-15 03:05:02 -0800

    4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join(['#' * self.__width] * self.__height) + '\n'
+        return '\n'.join(['#' * self.__width] * self.__height)
 
     def __repr__(self):
         """
