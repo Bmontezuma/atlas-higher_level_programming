3143abee05d4982b7dddbf3498d25fca2c839481
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-16 02:23:28 -0800

    Updated

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
 """
-import unittest
-from parameterized import parameterized
-from max_integer import max_integer
+Module to find the max integer in a list
+
+Example:
+    >>> max_integer([1, 2, 3, 4])
+    4
+    >>> max_integer([1, 3, 4, 2])
+    4
+    >>> max_integer([])
+    None
+    >>> max_integer([-5, -2, -8, -1])
+    -1
+    >>> max_integer([1, 4, 4, 3])
+    4
+"""
 
 
-class TestMaxInteger(unittest.TestCase):
-    """Test cases for max_integer function"""
+def max_integer(lst=[]):
+    """Function to find and return the max integer in a list of integers
+    If the list is empty, the function returns None
 
-    @parameterized.expand([
-        ([1, 2, 3, 4], 4),
-        ([1, 3, 4, 2], 4),
-        ([-1, -3, -4, -2], -1),
-        ([], None),
-        ([-1], -1),
-        ([0, 0, 0, 0], 0),
-        ([5, 5, 5, 5, 5, 5], 5),
-        ([1, 2, 3, 4, 5, 0], 5),
-        ([-5, -4, -3, -2, -1], -1),
-        ([1, 2, 3, 4, 5], 5),  # Test case: Max at the end
-        ([5, 1, 2, 3, 4], 5),  # Test case: Max at the beginning
-        ([1, 2, 5, 3, 4], 5),  # Test case: Max in the middle
-        ([-1, 2, 3, 4], 4),    # Test case: One negative number in the list
-        ([-1, -2, -3, -4], -1),  # Test case: Only negative numbers in the list
-        ([5], 5),              # Test case: List of one element
-        ([], None),            # Test case: List is empty
-    ])
-    def test_max_integer(self, input_list, expected_result):
-        """Test max_integer function with various input lists"""
-        result = max_integer(input_list)
-        self.assertEqual(result, expected_result)
+    Args:
+        lst (list): List of integers.
 
-
-if __name__ == '__main__':
-    unittest.main()
+    Returns:
+        int or None: The maximum integer in the list, or None if the list is empty.
+    """
+    if not lst:
+        return None
+    result = lst[0]
+    i = 1
+    while i < len(lst):
+        if lst[i] > result:
+            result = lst[i]
+        i += 1
+    return result
