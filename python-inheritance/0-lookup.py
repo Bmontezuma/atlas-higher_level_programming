59f3c87b9f6b48b489fd2193649834bc3f46dad7
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-18 03:27:12 -0800

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,31 +1,50 @@
-#!/usr/bin/python3
+"""
+This module provides a function for looking up attributes and methods of an object.
+"""
 
 def lookup(obj):
     """
-    Returns a list of available attributes and methods of an object.
+    This function takes an object as input and returns a list of its attributes and methods.
 
-    :param obj: The object to inspect.
-    :return: List of strings representing attributes and methods.
-
-    >>> class MyClass1(object):
-    ...     pass
-    >>> class MyClass2(object):
-    ...     my_attr1 = 3
-    ...     def my_meth(self):
-    ...         pass
-    >>> result = lookup(MyClass1)
-    >>> set(result) >= {'__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__'}
-    True
-    >>> result = lookup(MyClass2)
-    >>> set(result) >= {'__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'my_attr1', 'my_meth'}
-    True
-    >>> result = lookup(int)
-    >>> set(result) >= {'__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__gt__', '__hash__', '__index__', '__init__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes'}
-    True
+    :param obj: The input object.
+    :return: A list containing attributes and methods of the input object.
     """
     return dir(obj)
 
+# Additional test cases
+result_int = lookup(int)
+result_float = lookup(float)
+result_object = lookup(object)
+result_list = lookup(list)
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+class MyClassWithDirMethod:
+    """
+    A class with a custom dir method.
+    """
+    def dir(self):
+        """
+        Custom dir method returning a list.
+        """
+        return ["my_class", "is", "empty"]
+
+class MyClassEmpty:
+    """
+    An empty class.
+    """
+    pass
+
+class MyClassWithAttribute:
+    """
+    A class with one attribute.
+    """
+    one_attribute = 89
+
+class MyClassWithMethod:
+    """
+    A class with one method.
+    """
+    def one_method(self):
+        """
+        An example method.
+        """
+        pass
