ae524041a60ad83ff6768364dd1596ec9bc72f32
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-19 02:47:43 -0800

    5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry
+
+This module defines an empty class BaseGeometry.
+
+Usage:
+    BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+    bg = BaseGeometry()
+
+    print(bg)
+    print(dir(bg))
+    print(dir(BaseGeometry))
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing BaseGeometry.
+    """
+    pass
