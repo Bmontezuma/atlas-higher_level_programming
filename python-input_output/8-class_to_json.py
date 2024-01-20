808c798794db0b59d1bfb5121605fc291eab77d8
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-20 03:49:30 -0800

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+def class_to_json(obj):
+    """
+    Returns a dictionary for JSON serialization of an object.
+
+    Parameters:
+    - obj: Instance of a class with serializable attributes.
+
+    Returns:
+    dict: Dictionary for JSON serialization.
+
+    Example:
+    >>> class_to_json(MyClass("John"))
+    {'name': 'John', 'number': 0}
+    """
+    json_dict = {}
+    for key, value in obj.__dict__.items():
+        if not key.startswith("_") and not callable(value):
+            json_dict[key] = value if isinstance(value, (list, dict, str, int, bool)) \
+                else class_to_json(value)
+    return json_dict
