0f4e79d769f57a083e3a53165bfa9683e76fccce
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-19 17:11:44 -0800

    Updated

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,18 +6,19 @@ Module provides function for looking up attributes and methods of an object.
 
 def lookup(obj):
     """
-    Function takes object as input and returns a list of attributes and methods.
+    Function takes object input returns sorted list of attributes and methods.
 
     :param obj: The input object.
-    :return: A list containing attributes and methods of the input object.
+    :return: Sorted list containing attributes and methods of input object.
     """
-    return dir(obj)
+    return sorted(dir(obj))
 
-# Additional test cases
+
 result_int = lookup(int)
 result_float = lookup(float)
 result_object = lookup(object)
 result_list = lookup(list)
+
 
 class MyClassWithDirMethod:
     """
@@ -29,17 +30,20 @@ class MyClassWithDirMethod:
         """
         return ["my_class", "is", "empty"]
 
+
 class MyClassEmpty:
     """
     An empty class.
     """
     pass
 
+
 class MyClassWithAttribute:
     """
     A class with one attribute.
     """
     one_attribute = 89
+
 
 class MyClassWithMethod:
     """
