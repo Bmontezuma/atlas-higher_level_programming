c682a6bd3efb4a123510c932e2ddd2080fba02ef
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 15:35:44 -0800

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that defines class_to_json function
+"""
 
 
 def class_to_json(obj):
-    """
-    Returns a dictionary for JSON serialization of an object.
-
-    Parameters:
-    - obj: Instance of a class with serializable attributes.
-
-    Returns:
-    dict: Dictionary for JSON serialization.
-
-    Example:
-    >>> class_to_json(MyClass("John"))
-    {'name': 'John', 'number': 0}
-    """
-    json_dict = {}
-    for key, value in obj.__dict__.items():
-        if not key.startswith("_") and not callable(value):
-            json_dict[key] = value if isinstance(value, (list, dict, str, int, bool)) \
-                else class_to_json(value)
-    return json_dict
+    """Returns description with simple data structure for JSON serialization"""
+    return obj.__dict__
