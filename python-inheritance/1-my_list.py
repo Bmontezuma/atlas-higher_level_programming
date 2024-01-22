ad3de3339b539ebbea5a5995bf7fdc8a10429686
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 12:52:44 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,66 +1,27 @@
 #!/usr/bin/python3
-
 """
-This module defines a custom list class called MyList.
+Defines custom list class, MyList, that inherits from built-in list class.
+It includes a method, print_sorted, that prints the list in ascending order.
 """
 
 
-class MyList:
+class MyList(list):
     """
-    Custom List Class
+    Custom list class provides a method to print the list in ascending order.
 
-    Class represents list with methods to append items and print sorted list.
+    >>> my_list = MyList([1, 4, 2, 3, 5])
+    >>> my_list.print_sorted()
+    [1, 2, 3, 4, 5]
+    >>> my_list
+    [1, 4, 2, 3, 5]
     """
-
-    def __init__(self):
-        """
-        Initialize MyList with an empty list.
-
-        >>> ml = MyList()
-        >>> assert ml._list == []
-        """
-        self._list = []
-
-    def append(self, item):
-        """
-        Append an item to the list.
-
-        Args:
-            item (int): Item to append to the list.
-
-        >>> ml = MyList()
-        >>> ml.append(1)
-        >>> assert ml._list == [1]
-        """
-        self._list.append(item)
 
     def print_sorted(self):
         """
-        Sort the list and print it.
+        Prints the list in ascending order.
 
-        >>> ml = MyList()
-        >>> ml.append(1)
-        >>> ml.append(4)
-        >>> ml.append(2)
-        >>> ml.append(3)
-        >>> ml.append(5)
-        >>> ml.print_sorted()
+        >>> my_list = MyList([1, 4, 2, 3, 5])
+        >>> my_list.print_sorted()
         [1, 2, 3, 4, 5]
         """
-        sorted_list = sorted(self._list)
-        print(sorted_list)
-
-    def __str__(self):
-        """
-        Return the string representation of the list.
-
-        >>> ml = MyList()
-        >>> ml.append(1)
-        >>> ml.append(4)
-        >>> ml.append(2)
-        >>> ml.append(3)
-        >>> ml.append(5)
-        >>> print(ml)
-        [1, 4, 2, 3, 5]
-        """
-        return str(self._list)
+        print(sorted(self))
