19b2e5ac53dab09c95b97461cdeace82217f478e
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 13:30:26 -0800

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,28 +1,90 @@
 #!/usr/bin/python3
-from base_geometry import BaseGeometry
+"""
+Module: 8-rectangle
+
+This module defines a class Rectangle that inherits from BaseGeometry.
+
+Usage:
+    Rectangle = __import__('8-rectangle').Rectangle
+
+    # Example Usage
+    r = Rectangle(4, 5)
+    print(r)
+    print(r.area())
+
+    try:
+        r = Rectangle(2, "hello")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing BaseGeometry.
+
+    Public Methods:
+    - area(self): Raises Exception with message area() is not implemented.
+    - integer_validator(self, name, value): Validates the value.
+    """
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value.
+
+        Args:
+            name (str): The name of the value.
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
     """
-    This class represents a rectangle. It inherits from BaseGeometry.
-    """
+    A class representing Rectangle, inheriting from BaseGeometry.
 
+    Public Methods:
+    - __init__(self, width, height): Initializes Rectangle width and height.
+    """
     def __init__(self, width, height):
         """
-        Initializes a Rectangle object with width and height.
-        Both width and height must be positive integers.
-        """
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
+        Initializes a Rectangle instance with width and height.
 
-    def area(self):
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
         """
-        Returns the area of the rectangle.
-        """
-        return self.__width * self.__height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def __str__(self):
         """
         Returns a string representation of the rectangle.
+
+        Returns:
+            str: The rectangle description.
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        Computes and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
