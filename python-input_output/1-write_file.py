0e703397dcaff20800fc4a5dcb43e72ded76644e
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 14:59:36 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
-Module defines custom list class, MyList, that inherits from built-in list class in Python.
-It includes a method, print_sorted, that prints the list in ascending order.
+Module for writing content to a text file.
 """
 
 
-class MyList(list):
+def write_file(filename="", text=""):
     """
-    Custom list class that includes a method for printing the list in ascending order.
-    """
+    Writes given text to text file and returns number of characters.
 
-    def print_sorted(self):
-        """
-        Print the list in ascending order.
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+    Args:
+        filename (str): The name of the text file.
+        text (str): The text to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
+
+
+if __name__ == "__main__":
+    write_file("my_first_file.txt", "This School is so cool!\n")
