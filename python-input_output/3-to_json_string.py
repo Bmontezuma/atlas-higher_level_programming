2e513f68fb9b6b5353dcafb9fd848a22c9c8be8a
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 15:09:33 -0800

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
+"""
+Module for converting an object to its JSON representation (string).
+"""
+
 import json
 
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of an object (string).
+    Returns the JSON representation of an object as a string.
 
-    Parameters:
-    - my_obj: The object to be converted to a JSON string.
+    Args:
+        my_obj: The object to be converted to JSON.
 
     Returns:
-    str: JSON representation of the object.
-
-    Example:
-    >>> my_list = [1, 2, 3]
-    >>> to_json_string(my_list)
-    '[1, 2, 3]'
+        str: The JSON representation of the object.
     """
     return json.dumps(my_obj)
