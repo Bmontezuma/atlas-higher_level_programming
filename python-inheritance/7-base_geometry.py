5b14e83cd84d94c9d907fdefdca822fa128e1fc7
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 13:20:47 -0800

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,21 +9,8 @@ Usage:
 
     bg = BaseGeometry()
 
-    bg.integer_validator("my_int", 12)
-    bg.integer_validator("width", 89)
-
     try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
+        print(bg.area())
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
 """
@@ -35,7 +22,7 @@ class BaseGeometry:
 
     Public Methods:
     - area(self): Raises Exception with message area() is not implemented.
-    - integer_validator(self, name, value): Validates the integer value.
+    - integer_validator(self, name, value): Validates the value.
     """
     def area(self):
         """
@@ -45,17 +32,17 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """
-        Validates the integer value.
+        Validates the value.
 
         Args:
             name (str): The name of the value.
-            value: The value to validate.
+            value: The value to be validated.
 
         Raises:
             TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
+            ValueError: If the value is less or equal to 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
