62562ff306c7ac4363e574b1501d7509ac509108
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 15:59:15 -0800

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
+""" Module for Student class
+"""
+
 
 class Student:
-    """
-    Defines a student with first_name, last_name, and age attributes.
+    """ Student class
     """
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance.
-
-        Parameters:
-        - first_name (str): First name of the student.
-        - last_name (str): Last name of the student.
-        - age (int): Age of the student.
+        """ Initializes a Student instance
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
-
-        Parameters:
-        - attrs (list): List of strings representing attribute names to retrieve.
-
+        """ Retrieves a dictionary representation of a Student instance
+        Args:
+            attrs (list): List of strings specifying attribute names
         Returns:
-        dict: Dictionary representation of the Student instance.
+            dict: Dictionary representation of the Student instance
         """
-        if attrs is None:
-            return {'first_name': self.first_name, 'last_name': self.last_name, 'age': self.age}
-        else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return {attr: getattr(self, attr) for attr in attrs or dir(self) if isinstance(attr, str) and hasattr(self, attr)}
 
     def reload_from_json(self, json):
-        """
-        Replaces all attributes of the Student instance with the provided dictionary.
-
-        Parameters:
-        - json (dict): Dictionary containing attribute values.
+        """ Replaces all attributes of the Student instance
+        Args:
+            json (dict): Dictionary with attribute names and values
         """
         for key, value in json.items():
             setattr(self, key, value)
