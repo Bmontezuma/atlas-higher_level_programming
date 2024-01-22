cf3a37ac9f734d47d5c602e75d56f2c642109bfe
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 15:55:58 -0800

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
+"""
+Module that defines the Student class
+"""
+
 
 class Student:
-    """
-    Defines a student with first_name, last_name, and age attributes.
-    """
+    """Student class"""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance.
-
-        Parameters:
-        - first_name (str): First name of the student.
-        - last_name (str): Last name of the student.
-        - age (int): Age of the student.
-        """
+        """Initialize Student instance with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of a Student instance.
+        Retrieve dictionary representation of Student instance.
 
-        Parameters:
-        - attrs (list): List of strings representing attribute names to retrieve.
+        Args:
+            attrs (list): List specifying attribute names to retrieve.
 
         Returns:
-        dict: Dictionary representation of the Student instance.
+            dict: Dictionary of the Student instance.
         """
-        if attrs is None:
-            return {'first_name': self.first_name, 'last_name': self.last_name, 'age': self.age}
-        else:
+        if attrs is not None and all(isinstance(attr, str) for attr in attrs):
             return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
