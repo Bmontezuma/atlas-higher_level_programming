fc7d28b6d59c62e2017a3acdc978fcb03627164b
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 17:02:43 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 class MyList(list):
     """
     MyList inherits from list and provides additional functionality.
@@ -8,9 +10,14 @@ class MyList(list):
 
     Note: This class assumes that all elements in the list are of type int.
     """
+
     def print_sorted(self):
         """
         Prints the list in ascending order.
+
+        Returns:
+        - list: A new list containing elements in ascending order.
         """
         sorted_list = sorted(self)
         print("Sorted List:", sorted_list)
+        return sorted_list 
