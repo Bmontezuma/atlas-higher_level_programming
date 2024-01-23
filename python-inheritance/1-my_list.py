25652d40416b1fe3fc234f48fd36a9f963ff7c3d
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 16:14:17 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python3
 """
-Defines custom list class, MyList, that inherits from built-in list class.
-It includes a method, print_sorted, that prints the list in ascending order.
+1-my_list
+
+This module contains the definition of the MyList class.
 """
 
 
 class MyList(list):
     """
-    Custom list class provides a method to print the list in ascending order.
+    MyList class that inherits from list.
 
-    >>> my_list = MyList([1, 4, 2, 3, 5])
-    >>> my_list.print_sorted()
-    [1, 2, 3, 4, 5]
-    >>> my_list
-    [1, 4, 2, 3, 5]
+    Public Methods:
+    - print_sorted(self): Prints the list, but sorted (ascending sort).
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order.
+        Prints the list, but sorted (ascending sort).
 
-        >>> my_list = MyList([1, 4, 2, 3, 5])
+        Example:
+        >>> my_list = MyList()
+        >>> my_list.append(1)
+        >>> my_list.append(4)
+        >>> my_list.append(2)
+        >>> my_list.append(3)
+        >>> my_list.append(5)
+        >>> print(my_list)
+        [1, 4, 2, 3, 5]
         >>> my_list.print_sorted()
         [1, 2, 3, 4, 5]
+        >>> print(my_list)
+        [1, 4, 2, 3, 5]
         """
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
