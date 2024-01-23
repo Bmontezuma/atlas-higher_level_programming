b7dc6957a15fcc77c77a499879dc022ff0a3fa83
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-22 17:35:14 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
+"""
+Module defines a class `MyList` that inherits from built-in `list` class.
+It adds a method `print_sorted()` which prints the list in ascending order.
+
+# Test appending elements and printing sorted
+>>> my_list = MyList()
+>>> my_list.append(1)
+>>> my_list.append(4)
+>>> my_list.append(2)
+>>> my_list.append(3)
+>>> my_list.append(5)
+>>> print(my_list)
+[1, 4, 2, 3, 5]
+>>> my_list.print_sorted()
+Sorted List: [1, 2, 3, 4, 5]
+>>> print(my_list)
+[1, 4, 2, 3, 5]
+"""
 
 
 class MyList(list):
     """
-    MyList inherits from list and provides additional functionality.
-
-    Public methods:
-    - print_sorted(self): Prints the list in ascending order.
-
-    Note: This class assumes that all elements in the list are of type int.
+    This class inherits from built-in `list` class in Python.
+    It overrides the `__str__` method to return string representation of list.
+    It also adds method `print_sorted()` which prints list in ascending order.
     """
 
     def print_sorted(self):
-        """
-        Prints the list in ascending order.
-
-        Returns:
-        - list: A new list containing elements in ascending order.
-        """
+        """Prints the list in ascending order."""
         sorted_list = sorted(self)
         print("Sorted List:", sorted_list)
-        return sorted_list 
+
+    def __str__(self):
+        """Return a string representation of the list."""
+        return super().__str__()
