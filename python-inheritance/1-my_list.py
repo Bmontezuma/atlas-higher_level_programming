18e65345cd197f67605b83df69e4db5352174238
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-23 16:05:52 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,13 +5,19 @@ My List module
 
 
 class MyList(list):
-    """A custom list class that can print its elements in sorted order."""
+    """A custom list class that can print its elements in sorted order.
+
+    Public instance method:
+        def print_sorted(self): Prints the list, but sorted in ascending order.
+    """
 
     def print_sorted(self):
         """Prints the elements of the list in ascending sorted order."""
         sorted_list = sorted(self)
         print(sorted_list)
 
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("test_my_list.txt", verbose=True, optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt", verbose=True,
+                     optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
