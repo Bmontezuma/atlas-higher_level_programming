d2027a6fbe394cef4180466780f8a0db6ac85055
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-26 01:46:03 -0800

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,46 @@
 #!/usr/bin/python3
-"""This is my Base module"""
+"""
+This module defines the Base class.
+"""
 
 
 class Base:
-    """The Base class for managing IDs"""
+    """
+    Represents the base class.
+
+    Attributes:
+        __nb_objects (int): Represents the number of objects instantiated.
+        id (int): The identifier of the instance.
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize Base instance"""
+        """
+        Initialize a Base instance.
+
+        Args:
+            id (int, optional): The identifier of the instance.
+
+        Examples:
+            >>> b1 = Base()
+            >>> b1.id
+            1
+
+            >>> b2 = Base()
+            >>> b2.id
+            2
+
+            >>> b3 = Base()
+            >>> b3.id
+            3
+
+            >>> b4 = Base(12)
+            >>> b4.id
+            12
+        """
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
