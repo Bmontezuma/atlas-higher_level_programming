a02d0b9258ed83978d4510878f8eada6d1b091f6
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-26 02:44:32 -0800

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,8 @@ class Rectangle(Base):
         """Print the rectangle with '#' characters"""
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """Return the string representation of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
