024b54a0c59c2a7c075c46397a5ae5062e50b90a
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2024-01-27 05:31:28 -0800

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,8 +95,13 @@ class Rectangle(Base):
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
-            print(' ' * self.__x, end='')
-            print('#' * self.__width)
+            print(' ' * self.__x + '#' * self.__width)
+
+    def update(self, *args):
+        """Update attributes with arguments"""
+        attrs = ["id", "width", "height", "x", "y"]
+        for i, arg in enumerate(args):
+            setattr(self, attrs[i], arg)
 
     def __str__(self):
         """Return the string representation of the rectangle"""
