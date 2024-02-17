abf2cc98200d83832d7928f2dad744b917f92bd8
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-17 12:05:09 +0000

    4-never_empty.sql

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- Task: Create the table id_not_null with specified description
+
+-- Create the table id_not_null if it does not exist
+CREATE TABLE IF NOT EXISTS id_not_null
+(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
