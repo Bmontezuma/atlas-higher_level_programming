ff47428e1df7868a4943cc1ee6b02a0d24909046
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-18 13:39:10 +0000

    Updated 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,15 @@
--- Retrieve the state_id for California using a subquery
-SELECT id FROM states WHERE name = 'California';
+-- Retrieve the state_id for California
+SELECT id 
+FROM states 
+WHERE name = 'California';
 
 -- Use the retrieved state_id to select cities of California
 SELECT id, name
 FROM cities
 WHERE state_id = (
-    SELECT id FROM states WHERE name = 'California'
+    -- Subquery to get the state_id for California
+    SELECT id 
+    FROM states 
+    WHERE name = 'California'
 )
 ORDER BY id ASC;
