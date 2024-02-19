58e60510845df9f48a74bfdeda1fe935e67290bf
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 23:21:03 +0000

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,17 +1,15 @@
--- Task: Create the database hbtn_0d_usa and the table states with specified description
+-- Script Description: Creates the database hbtn_0d_usa and the table states if they don't already exist
 
--- Create the database hbtn_0d_usa if it does not exist
+-- Query to create the database hbtn_0d_usa if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Use the hbtn_0d_usa database
+-- Query to use the database hbtn_0d_usa
 USE hbtn_0d_usa;
 
--- Create the table states if it does not exist
-CREATE TABLE IF NOT EXISTS states
-(
+-- Query to create the table states if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
 
--- Insert data into the states table
-INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas');
+-- Expected output will be the execution without errors
