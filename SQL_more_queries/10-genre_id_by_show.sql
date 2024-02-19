68eab8a54317aca4ef57670fcbb8a3fbc8cf491c
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 23:27:17 +0000

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- List all shows with at least one genre linked
+-- Script Description: Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+
+-- Query to select shows and their linked genres
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
