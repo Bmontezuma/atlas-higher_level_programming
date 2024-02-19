4547636a72f0ded4ee70b80f0ec1b328578895e5
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 00:48:54 +0000

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument (safe from MySQL injection).
+Script that takes in arguments and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument (safe from MySQL injection).
 """
 
 import sys
