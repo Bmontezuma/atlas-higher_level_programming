52ac873ca83e2fe6836b1c006eb95c51e6fc9436
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 23:30:41 +0000

    11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,7 @@
--- List all shows with their corresponding genre IDs
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL) AS genre_id
+-- Script Description: Lists all shows contained in the database hbtn_0d_tvshows
+
+-- Query to select shows and their linked genres
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
