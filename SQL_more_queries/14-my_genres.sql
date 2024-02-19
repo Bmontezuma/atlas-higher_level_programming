8247da1e4ef9e2e63c889cd17e9b646133c2ccb8
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 23:39:51 +0000

    14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
--- List all genres of the show Dexter
+-- Script Description: Lists all genres of the show Dexter from the hbtn_0d_tvshows database
+
+-- Query to select genres of the show Dexter
 SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
