0b2c969d7dcd6119fb648f6989bab701578099f3
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 01:07:27 +0000

    Updated Files

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all cities from the database hbtn_0e_4_usa.
+Script that takes in the name of a state as an argument
+and lists all cities of that state.
 """
 
 import sys
@@ -9,12 +10,13 @@ import MySQLdb
 
 def main():
     """
-    Main function to retrieve and display cities from the database.
+    Main function to retrieve and display cities for a given state.
     """
-    # Retrieving MySQL credentials from command line arguments
+    # Retrieving MySQL credentials and state name from command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connecting to MySQL server
     db = MySQLdb.connect(user=username, passwd=password, db=database)
@@ -22,22 +24,27 @@ def main():
     # Creating a cursor object using cursor() method
     cursor = db.cursor()
 
-    # Executing SQL query to select all cities with corresponding state names
+    # Executing SQL query to select all cities for the given state
     query = """
-            SELECT c.id, c.name, s.name
-            FROM cities AS c
-            INNER JOIN states AS s
-            ON c.state_id = s.id
-            ORDER BY c.id
+            SELECT cities.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id
             """
-    cursor.execute(query)
+    cursor.execute(query, (state_name,))
 
     # Fetching all rows from the result set
     rows = cursor.fetchall()
 
+    # Extracting city names from the rows
+    cities = [row[0] for row in rows]
+
     # Displaying results
-    for row in rows:
-        print(row)
+    if cities:
+        print(", ".join(cities))
+    else:
+        print()
 
     # Closing cursor and database connection
     cursor.close()
