0d542b6078ba97abab52e37c28e61390757427bb
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-19 00:07:16 +0000

    Updated 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,10 +5,17 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py <username> <password> <db_name>")
+        sys.exit(1)
+
     # Connect to MySQL server
     db = MySQLdb.connect(
-        host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-        db=sys.argv[3], port=3306
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
     )
 
     # Create a cursor object
