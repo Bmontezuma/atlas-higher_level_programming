75e14e992745987671b2e088e599b519874c9918
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-21 09:26:18 +0000

    0-javascript_is_amazing.js

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -2,13 +2,11 @@
 
 /**
  * Script to print a message "JavaScript is amazing"
- * 
- * This script creates a constant variable 
+ * This script creates a constant variable
  * myVar with the value "JavaScript is amazing"
  * and then prints it using console.log()
- * 
  * Usage: ./0-javascript_is_amazing.js
- */
+ * */
 
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
