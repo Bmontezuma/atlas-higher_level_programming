a831a16f55b962977d40191cacc23388dbb6ebca
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-21 09:29:49 +0000

    1-multi_languages.js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/**
+ * Script to print three lines in different programming languages
+ *
+ * This script prints three lines:
+ *  - The first line: "C is fun"
+ *  - The second line: "Python is cool"
+ *  - The third line: "JavaScript is amazing"
+ *
+ * Usage: ./1-multi_languages.js
+ */
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
