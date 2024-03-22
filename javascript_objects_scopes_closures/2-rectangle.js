c36f47856c37bd11ce5e67421dbda6b7df1d131b
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-22 09:21:46 +0000

    2-rectangle.js

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      // If width or height is not a positive integer or is 0, create an empty object
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
