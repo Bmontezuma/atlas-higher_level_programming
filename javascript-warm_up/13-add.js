10337ec4ac750b9753ae67864c104f239c79cd70
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-22 08:44:01 +0000

    13-add.js

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * Function to add two integers
+ * @param {number} a - The first integer
+ * @param {number} b - The second integer
+ * @returns {number} The addition of a and b
+ */
+function add (a, b) {
+  return a + b;
+}
+
+module.exports = { add };
