3933c8a13e3012a3a7944faef559bd086ee55482
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-22 09:27:58 +0000

    6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    const line = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Square;
