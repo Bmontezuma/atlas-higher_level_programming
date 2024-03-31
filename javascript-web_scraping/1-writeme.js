bff7c6e1c113f0cbc383a335db664341ff2e5c46
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-31 00:38:58 +0000

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,22 +1,19 @@
-#!/usr/bin/node
-
 const fs = require('fs');
 
-// Check if both file path and string arguments are provided
+// Check if correct number of arguments are passed
 if (process.argv.length !== 4) {
-  console.log('Usage: ./1-writeme.js <file-path> <string-to-write>');
+  console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
   process.exit(1);
 }
 
-// Get the file path and string to write from command line arguments
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
+// Extract arguments
+const [, , filePath, content] = process.argv;
 
-// Write the string to the file
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+// Write content to file
+fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(stringToWrite);
+    return;
   }
+  console.log(`Content has been written to ${filePath}`);
 });
