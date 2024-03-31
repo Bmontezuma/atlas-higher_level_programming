6cee537523d25f2c002e95c4bf9c722f1dad40d4
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-31 00:47:33 +0000

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs');
+
+const fs = require('fs').promises;
 
 // Check if correct number of arguments are passed
 if (process.argv.length !== 4) {
@@ -11,10 +12,14 @@ if (process.argv.length !== 4) {
 const [, , filePath, content] = process.argv;
 
 // Write content to file
-fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
-  if (err) {
+async function writeToFile () {
+  try {
+    await fs.writeFile(filePath, content, { encoding: 'utf-8' });
+    const data = await fs.readFile(filePath, { encoding: 'utf-8' });
+    console.log(data);
+  } catch (err) {
     console.error(err);
-    return;
   }
-  console.log(content); // Output the content directly
-});
+}
+
+writeToFile();
